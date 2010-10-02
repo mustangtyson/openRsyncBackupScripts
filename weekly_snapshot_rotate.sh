50f60165d093e9ccd356306e61594921296fa8f5
Tyson Smith <tysonsmith@gmail.com>
2010-10-02 15:21:58 -0500

    added subdirs

--- weekly_snapshot_rotate.sh
+++ weekly_snapshot_rotate.sh
@@ -11,6 +11,7 @@ MV=/bin/mv;
 CP=/bin/cp;
 TOUCH=/bin/touch;
 RSYNC=/usr/bin/rsync;
+MKDIR=/bin/mkdir
 
 # ------------- file locations -----------------------------------------
 SNAPSHOT_RW=/root/backups/snapshot;
@@ -20,36 +21,37 @@ SNAPSHOT_RW=/root/backups/snapshot;
 if (( `$ID -u` != 0 )); then { $ECHO "Sorry, must be root.  Exiting..."; exit 1; } fi
 
 # step 1: delete the oldest snapshot, if it exists:
-if [ -d $SNAPSHOT_RW/weekly.4 ] ; then			\
-	$RM -rf $SNAPSHOT_RW/weekly.4 ;				\
+if [ -d $SNAPSHOT_RW/weekly/weekly.4 ] ; then			\
+	$RM -rf $SNAPSHOT_RW/weekly/weekly.4 ;				\
 fi ;
 
 # step 2: shift the middle snapshots(s) back by one, if they exist
-if [ -d $SNAPSHOT_RW/weekly.3 ] ; then			\
-	$MV $SNAPSHOT_RW/weekly.3 $SNAPSHOT_RW/weekly.4 ;	\
+if [ -d $SNAPSHOT_RW/weekly/weekly.3 ] ; then			\
+	$MV $SNAPSHOT_RW/weekly/weekly.3 $SNAPSHOT_RW/weekly/weekly.4 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/weekly.2 ] ; then			\
-	$MV $SNAPSHOT_RW/weekly.2 $SNAPSHOT_RW/weekly.3 ;	\
+if [ -d $SNAPSHOT_RW/weekly/weekly.2 ] ; then			\
+	$MV $SNAPSHOT_RW/weekly/weekly.2 $SNAPSHOT_RW/weekly/weekly.3 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/weekly.1 ] ; then			\
-	$MV $SNAPSHOT_RW/weekly.1 $SNAPSHOT_RW/weekly.2 ;	\
+if [ -d $SNAPSHOT_RW/weekly/weekly.1 ] ; then			\
+	$MV $SNAPSHOT_RW/weekly/weekly.1 $SNAPSHOT_RW/weekly/weekly.2 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/weekly.0 ] ; then			\
-	$MV $SNAPSHOT_RW/weekly.0 $SNAPSHOT_RW/weekly.1
+if [ -d $SNAPSHOT_RW/weekly/weekly.0 ] ; then			\
+	$MV $SNAPSHOT_RW/weekly/weekly.0 $SNAPSHOT_RW/weekly/weekly.1
 fi;
 
 # step 3: make a hard-link-only (except for dirs) copy of the latest snapshot,
 # if that exists
+${MKDIR} -p $SNAPSHOT_RW/weekly
 ${CP} \
 	-al \
-	$SNAPSHOT_RW/daily.7 \
-        $SNAPSHOT_RW/weekly.0 ;
+	$SNAPSHOT_RW/daily/daily.7 \
+        $SNAPSHOT_RW/weekly/weekly.0 ;
 
 # step 5: update the mtime of weekly.0 to reflect the snapshot time
-$TOUCH $SNAPSHOT_RW/weekly.0 ;
+$TOUCH $SNAPSHOT_RW/weekly/weekly.0 ;
 
 exit 0
 
