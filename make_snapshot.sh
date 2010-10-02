05667c6e781486d2becb382877f244fb52c1a38c
Tyson Smith <tysonsmith@gmail.com>
2010-10-02 15:05:02 -0500

    now is in a subdir

--- make_snapshot.sh
+++ make_snapshot.sh
@@ -22,38 +22,40 @@ BACKUP_RW=/root/backups/backup;
 if (( `$ID -u` != 0 )); then { $ECHO "Sorry, must be root.  Exiting..."; exit 1; } fi
 
 # step 1: delete the oldest snapshot, if it exists:
-if [ -d $SNAPSHOT_RW/hourly.3 ] ; then			\
-	$RM -rf $SNAPSHOT_RW/hourly.3 ;				\
+if [ -d $SNAPSHOT_RW/hourly/hourly.3 ] ; then			\
+	$RM -rf $SNAPSHOT_RW/hourly/hourly.3 ;				\
 fi ;
 
 # step 2: shift the middle snapshots(s) back by one, if they exist
-if [ -d $SNAPSHOT_RW/hourly.2 ] ; then			\
-	$MV $SNAPSHOT_RW/hourly.2 $SNAPSHOT_RW/hourly.3 ;	\
+if [ -d $SNAPSHOT_RW/hourly/hourly.2 ] ; then			\
+	$MV $SNAPSHOT_RW/hourly/hourly.2 $SNAPSHOT_RW/hourly/hourly.3 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/hourly.1 ] ; then			\
-	$MV $SNAPSHOT_RW/hourly.1 $SNAPSHOT_RW/hourly.2 ;	\
+if [ -d $SNAPSHOT_RW/hourly/hourly.1 ] ; then			\
+	$MV $SNAPSHOT_RW/hourly/hourly.1 $SNAPSHOT_RW/hourly/hourly.2 ;	\
 fi;
 
 # step 3: make a hard-link-only (except for dirs) copy of the latest snapshot,
 # if that exists
-if [ -d $SNAPSHOT_RW/hourly.0 ] ; then			\
-	$MV $SNAPSHOT_RW/hourly.0 $SNAPSHOT_RW/hourly.1
+if [ -d $SNAPSHOT_RW/hourly/hourly.0 ] ; then			\
+	$MV $SNAPSHOT_RW/hourly/hourly.0 $SNAPSHOT_RW/hourly/hourly.1; \
 fi;
 
 # step 4: rsync from the system into the latest snapshot (notice that
 # rsync behaves like cp --remove-destination by default, so the destination
 # is unlinked first.  If it were not so, this would copy over the other
 # snapshot(s) too!
+$MKDIR -p $SNAPSHOT_RW/hourly/hourly.0;	\
+
 $RSYNC \
 	-a \
 	--delete \
-        --link-dest=$SNAPSHOT_RW/hourly.1 \
+        --link-dest=$SNAPSHOT_RW/hourly/hourly.1 \
 	${BACKUP_RW}/ \
-        $SNAPSHOT_RW/hourly.0/ ;
+        $SNAPSHOT_RW/hourly/hourly.0/ ;
 
 # step 5: update the mtime of hourly.0 to reflect the snapshot time
-$TOUCH $SNAPSHOT_RW/hourly.0 ;
+$TOUCH $SNAPSHOT_RW/hourly/hourly.0 ;
 
 exit 0
 
