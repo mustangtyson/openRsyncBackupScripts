429456f6dbb5cb8d508bf46bfae34a75eebba95b
Tyson Smith <tysonsmith@gmail.com>
2010-10-02 15:34:36 -0500

    subdirs

--- yearly_snapshot_rotate.sh
+++ yearly_snapshot_rotate.sh
@@ -11,6 +11,7 @@ MV=/bin/mv;
 CP=/bin/cp;
 TOUCH=/bin/touch;
 RSYNC=/usr/bin/rsync;
+MKDIR=/bin/mkdir
 
 # ------------- file locations -----------------------------------------
 SNAPSHOT_RW=/root/backups/snapshot;
@@ -20,68 +21,69 @@ SNAPSHOT_RW=/root/backups/snapshot;
 if (( `$ID -u` != 0 )); then { $ECHO "Sorry, must be root.  Exiting..."; exit 1; } fi
 
 # step 1: delete the oldest snapshot, if it exists:
-if [ -d $SNAPSHOT_RW/yearly.12 ] ; then			\
-	$RM -rf $SNAPSHOT_RW/yearly.12 ;				\
+if [ -d $SNAPSHOT_RW/yearly/yearly.12 ] ; then			\
+	$RM -rf $SNAPSHOT_RW/yearly/yearly.12 ;				\
 fi ;
 
 # step 2: shift the middle snapshots(s) back by one, if they exist
-if [ -d $SNAPSHOT_RW/yearly.11 ] ; then			\
-	$MV $SNAPSHOT_RW/yearly.11 $SNAPSHOT_RW/yearly.12 ;	\
+if [ -d $SNAPSHOT_RW/yearly/yearly.11 ] ; then			\
+	$MV $SNAPSHOT_RW/yearly/yearly.11 $SNAPSHOT_RW/yearly/yearly.12 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/yearly.10 ] ; then			\
-	$MV $SNAPSHOT_RW/yearly.10 $SNAPSHOT_RW/yearly.11 ;	\
+if [ -d $SNAPSHOT_RW/yearly/yearly.10 ] ; then			\
+	$MV $SNAPSHOT_RW/yearly/yearly.10 $SNAPSHOT_RW/yearly/yearly.11 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/yearly.9 ] ; then			\
-	$MV $SNAPSHOT_RW/yearly.9 $SNAPSHOT_RW/yearly.10 ;	\
+if [ -d $SNAPSHOT_RW/yearly/yearly.9 ] ; then			\
+	$MV $SNAPSHOT_RW/yearly/yearly.9 $SNAPSHOT_RW/yearly/yearly.10 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/yearly.8 ] ; then			\
-	$MV $SNAPSHOT_RW/yearly.8 $SNAPSHOT_RW/yearly.9 ;	\
+if [ -d $SNAPSHOT_RW/yearly/yearly.8 ] ; then			\
+	$MV $SNAPSHOT_RW/yearly/yearly.8 $SNAPSHOT_RW/yearly/yearly.9 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/yearly.7 ] ; then			\
-	$MV $SNAPSHOT_RW/yearly.7 $SNAPSHOT_RW/yearly.8 ;	\
+if [ -d $SNAPSHOT_RW/yearly/yearly.7 ] ; then			\
+	$MV $SNAPSHOT_RW/yearly/yearly.7 $SNAPSHOT_RW/yearly/yearly.8 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/yearly.6 ] ; then			\
-	$MV $SNAPSHOT_RW/yearly.6 $SNAPSHOT_RW/yearly.7 ;	\
+if [ -d $SNAPSHOT_RW/yearly/yearly.6 ] ; then			\
+	$MV $SNAPSHOT_RW/yearly/yearly.6 $SNAPSHOT_RW/yearly/yearly.7 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/yearly.5 ] ; then			\
-	$MV $SNAPSHOT_RW/yearly.5 $SNAPSHOT_RW/yearly.6 ;	\
+if [ -d $SNAPSHOT_RW/yearly/yearly.5 ] ; then			\
+	$MV $SNAPSHOT_RW/yearly/yearly.5 $SNAPSHOT_RW/yearly/yearly.6 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/yearly.4 ] ; then			\
-	$MV $SNAPSHOT_RW/yearly.4 $SNAPSHOT_RW/yearly.5 ;	\
+if [ -d $SNAPSHOT_RW/yearly/yearly.4 ] ; then			\
+	$MV $SNAPSHOT_RW/yearly/yearly.4 $SNAPSHOT_RW/yearly/yearly.5 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/yearly.3 ] ; then			\
-	$MV $SNAPSHOT_RW/yearly.3 $SNAPSHOT_RW/yearly.4 ;	\
+if [ -d $SNAPSHOT_RW/yearly/yearly.3 ] ; then			\
+	$MV $SNAPSHOT_RW/yearly/yearly.3 $SNAPSHOT_RW/yearly/yearly.4 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/yearly.2 ] ; then			\
-	$MV $SNAPSHOT_RW/yearly.2 $SNAPSHOT_RW/yearly.3 ;	\
+if [ -d $SNAPSHOT_RW/yearly/yearly.2 ] ; then			\
+	$MV $SNAPSHOT_RW/yearly/yearly.2 $SNAPSHOT_RW/yearly/yearly.3 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/yearly.1 ] ; then			\
-	$MV $SNAPSHOT_RW/yearly.1 $SNAPSHOT_RW/yearly.2 ;	\
+if [ -d $SNAPSHOT_RW/yearly/yearly.1 ] ; then			\
+	$MV $SNAPSHOT_RW/yearly/yearly.1 $SNAPSHOT_RW/yearly/yearly.2 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/yearly.0 ] ; then			\
-	$MV $SNAPSHOT_RW/yearly.0 $SNAPSHOT_RW/yearly.1
+if [ -d $SNAPSHOT_RW/yearly/yearly.0 ] ; then			\
+	$MV $SNAPSHOT_RW/yearly/yearly.0 $SNAPSHOT_RW/yearly/yearly.1
 fi;
 
 # step 3: make a hard-link-only (except for dirs) copy of the latest snapshot,
 # if that exists
+${MKDIR} -p $SNAPSHOT_RW/yearly
 ${CP} \
 	-al \
-	$SNAPSHOT_RW/monthly.12 \
-        $SNAPSHOT_RW/yearly.0 ;
+	$SNAPSHOT_RW/monthly/monthly.12 \
+        $SNAPSHOT_RW/yearly/yearly.0 ;
 
 # step 5: update the mtime of yearly.0 to reflect the snapshot time
-$TOUCH $SNAPSHOT_RW/yearly.0 ;
+$TOUCH $SNAPSHOT_RW/yearly/yearly.0 ;
 
 exit 0
 
