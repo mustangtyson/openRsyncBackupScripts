182d2c532e74d2aebd92f6b8d5fb6cafe717d038
Tyson Smith <tysonsmith@gmail.com>
2010-10-02 15:30:27 -0500

    subdirs

--- monthly_snapshot_rotate.sh
+++ monthly_snapshot_rotate.sh
@@ -11,6 +11,7 @@ MV=/bin/mv;
 CP=/bin/cp;
 TOUCH=/bin/touch;
 RSYNC=/usr/bin/rsync;
+MKDIR=/bin/mkdir
 
 # ------------- file locations -----------------------------------------
 SNAPSHOT_RW=/root/backups/snapshot;
@@ -20,68 +21,69 @@ SNAPSHOT_RW=/root/backups/snapshot;
 if (( `$ID -u` != 0 )); then { $ECHO "Sorry, must be root.  Exiting..."; exit 1; } fi
 
 # step 1: delete the oldest snapshot, if it exists:
-if [ -d $SNAPSHOT_RW/monthly.12 ] ; then			\
-	$RM -rf $SNAPSHOT_RW/monthly.12 ;				\
+if [ -d $SNAPSHOT_RW/monthly/monthly.12 ] ; then			\
+	$RM -rf $SNAPSHOT_RW/monthly/monthly.12 ;				\
 fi ;
 
 # step 2: shift the middle snapshots(s) back by one, if they exist
-if [ -d $SNAPSHOT_RW/monthly.11 ] ; then			\
-	$MV $SNAPSHOT_RW/monthly.11 $SNAPSHOT_RW/monthly.12 ;	\
+if [ -d $SNAPSHOT_RW/monthly/monthly.11 ] ; then			\
+	$MV $SNAPSHOT_RW/monthly/monthly.11 $SNAPSHOT_RW/monthly/monthly.12 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/monthly.10 ] ; then			\
-	$MV $SNAPSHOT_RW/monthly.10 $SNAPSHOT_RW/monthly.11 ;	\
+if [ -d $SNAPSHOT_RW/monthly/monthly.10 ] ; then			\
+	$MV $SNAPSHOT_RW/monthly/monthly.10 $SNAPSHOT_RW/monthly/monthly.11 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/monthly.9 ] ; then			\
-	$MV $SNAPSHOT_RW/monthly.9 $SNAPSHOT_RW/monthly.10 ;	\
+if [ -d $SNAPSHOT_RW/monthly/monthly.9 ] ; then			\
+	$MV $SNAPSHOT_RW/monthly/monthly.9 $SNAPSHOT_RW/monthly/monthly.10 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/monthly.8 ] ; then			\
-	$MV $SNAPSHOT_RW/monthly.8 $SNAPSHOT_RW/monthly.9 ;	\
+if [ -d $SNAPSHOT_RW/monthly/monthly.8 ] ; then			\
+	$MV $SNAPSHOT_RW/monthly/monthly.8 $SNAPSHOT_RW/monthly/monthly.9 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/monthly.7 ] ; then			\
-	$MV $SNAPSHOT_RW/monthly.7 $SNAPSHOT_RW/monthly.8 ;	\
+if [ -d $SNAPSHOT_RW/monthly/monthly.7 ] ; then			\
+	$MV $SNAPSHOT_RW/monthly/monthly.7 $SNAPSHOT_RW/monthly/monthly.8 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/monthly.6 ] ; then			\
-	$MV $SNAPSHOT_RW/monthly.6 $SNAPSHOT_RW/monthly.7 ;	\
+if [ -d $SNAPSHOT_RW/monthly/monthly.6 ] ; then			\
+	$MV $SNAPSHOT_RW/monthly/monthly.6 $SNAPSHOT_RW/monthly/monthly.7 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/monthly.5 ] ; then			\
-	$MV $SNAPSHOT_RW/monthly.5 $SNAPSHOT_RW/monthly.6 ;	\
+if [ -d $SNAPSHOT_RW/monthly/monthly.5 ] ; then			\
+	$MV $SNAPSHOT_RW/monthly/monthly.5 $SNAPSHOT_RW/monthly/monthly.6 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/monthly.4 ] ; then			\
-	$MV $SNAPSHOT_RW/monthly.4 $SNAPSHOT_RW/monthly.5 ;	\
+if [ -d $SNAPSHOT_RW/monthly/monthly.4 ] ; then			\
+	$MV $SNAPSHOT_RW/monthly/monthly.4 $SNAPSHOT_RW/monthly/monthly.5 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/monthly.3 ] ; then			\
-	$MV $SNAPSHOT_RW/monthly.3 $SNAPSHOT_RW/monthly.4 ;	\
+if [ -d $SNAPSHOT_RW/monthly/monthly.3 ] ; then			\
+	$MV $SNAPSHOT_RW/monthly/monthly.3 $SNAPSHOT_RW/monthly/monthly.4 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/monthly.2 ] ; then			\
-	$MV $SNAPSHOT_RW/monthly.2 $SNAPSHOT_RW/monthly.3 ;	\
+if [ -d $SNAPSHOT_RW/monthly/monthly.2 ] ; then			\
+	$MV $SNAPSHOT_RW/monthly/monthly.2 $SNAPSHOT_RW/monthly/monthly.3 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/monthly.1 ] ; then			\
-	$MV $SNAPSHOT_RW/monthly.1 $SNAPSHOT_RW/monthly.2 ;	\
+if [ -d $SNAPSHOT_RW/monthly/monthly.1 ] ; then			\
+	$MV $SNAPSHOT_RW/monthly/monthly.1 $SNAPSHOT_RW/monthly/monthly.2 ;	\
 fi;
 
-if [ -d $SNAPSHOT_RW/monthly.0 ] ; then			\
-	$MV $SNAPSHOT_RW/monthly.0 $SNAPSHOT_RW/monthly.1
+if [ -d $SNAPSHOT_RW/monthly/monthly.0 ] ; then			\
+	$MV $SNAPSHOT_RW/monthly/monthly.0 $SNAPSHOT_RW/monthly/monthly.1
 fi;
 
 # step 3: make a hard-link-only (except for dirs) copy of the latest snapshot,
 # if that exists
+${MKDIR} -p $SNAPSHOT_RW/monthly
 ${CP} \
 	-al \
-	$SNAPSHOT_RW/weekly.4 \
-        $SNAPSHOT_RW/monthly.0 ;
+	$SNAPSHOT_RW/weekly/weekly.4 \
+        $SNAPSHOT_RW/monthly/monthly.0 ;
 
 # step 5: update the mtime of monthly.0 to reflect the snapshot time
-$TOUCH $SNAPSHOT_RW/monthly.0 ;
+$TOUCH $SNAPSHOT_RW/monthly/monthly.0 ;
 
 exit 0
 
