f3bd026901a64efed7be305eb00c21f58968b267
Tyson Smith <tysonsmith@gmail.com>
2010-10-02 22:26:41 -0500

    working on getting clients to rsync to the server

---
+++ scripts/backup_clients.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+RSYNC=/usr/bin/rsync
+SSH=/usr/bin/ssh
+MKDIR=/bin/mkdir
+
+BACKUP_DIR=/root/backups/backup #You must set this to your backup path
+
+# Figure out where to save the backup
+LOCAL_PATH=${BACKUP_DIR}/${RHOST}/
+
+# Make sure the dir to backup to exists
+${MKDIR} -p ${LOCAL_PATH}
+
+# Perform the backup
+${RSYNC} -az -e ${SSH} ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH} ${LOCAL_PATH}
+
+exit 0
