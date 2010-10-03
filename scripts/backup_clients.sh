14ec0fbefc15105f056d60af990947a36c93ffd7
backupuser <backupuser@dalserver.(none)>
2010-10-02 23:17:29 -0500

    working better now

--- scripts/backup_clients.sh
+++ scripts/backup_clients.sh
@@ -5,13 +5,15 @@ MKDIR=/bin/mkdir
 
 BACKUP_DIR=/mnt/backups/backup #TODO You must set this to your backup path
 
+. ../config/clients/tsmith@wopr
+
 # Figure out where to save the backup
-LOCAL_PATH=${BACKUP_DIR}/${RHOST}/
+LOCAL_PATH=${BACKUP_DIR}/${REMOTE_HOST}
 
 # Make sure the dir to backup to exists
-${MKDIR} -p ${LOCAL_PATH}
+${MKDIR} -p ${LOCAL_PATH}${REMOTE_PATH}
 
 # Perform the backup
-${RSYNC} -az -e ${SSH} ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH} ${LOCAL_PATH}
+${RSYNC} -az -e ${SSH} ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH} ${LOCAL_PATH}${REMOTE_PATH}/../
 
 exit 0
