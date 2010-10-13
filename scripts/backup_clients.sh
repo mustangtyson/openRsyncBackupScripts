6caeafc4d5f241b935ce6aa083ba41a381404b72
Tyson Smith <tysonsmith@gmail.com>
2010-10-12 21:20:23 -0500

    make sure that the script is not already running

--- scripts/backup_clients.sh
+++ scripts/backup_clients.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
+# System executables
 RSYNC=/usr/bin/rsync
 SSH=/usr/bin/ssh
 MKDIR=/bin/mkdir
 
+# Lock file to make sure that the script is not running more than once.
+lf=/tmp/openRsyncBackupScriptsPidLockFile
+
+# User editable variables
 BACKUP_DIR=/mnt/backups/backup #TODO You must set this to your backup path
 
 
+# Make sure that we are not already running
+touch $lf
+read lastPID < $lf
+echo "$lastPID"
+[ ! -z "$lastPID" -a -d /proc/$lastPID ] && exit 1
+
+# Start of the backups
 cd ~/openRsyncBackupScripts/config/clients
 
 for file in `dir -d *` ; do
 	echo "Back up ${file} started"
 
-	# TODO CLEAR THE VARIABLES
+	# CLEAR THE VARIABLES
+	# TODO
 
 	# Import the files variables
 	. ${file}
