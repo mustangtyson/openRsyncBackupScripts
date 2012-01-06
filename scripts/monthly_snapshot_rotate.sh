68878fc47958dcafcb4d7414cfc561a3b5e4d90f
Tyson Smith <tysonsmith@gmail.com>
2012-01-06 16:20:58 -0600

    now works in cron was not setting path to config files correctly

--- scripts/monthly_snapshot_rotate.sh
+++ scripts/monthly_snapshot_rotate.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-source ./OrbsUtils.sh
-source ../config/orbs.conf
-
 # ------------- system commands used by this script --------------------
 ID=/usr/bin/id;
 ECHO=/bin/echo;
@@ -13,14 +10,44 @@ MV=/bin/mv;
 CP=/bin/cp;
 TOUCH=/bin/touch;
 RSYNC=/usr/bin/rsync;
-MKDIR=/bin/mkdir
-
+MKDIR=/bin/mkdir;
+ORBS_UTILS="OrbsUtils.sh"
+ORBS_CONF="orbs.conf"
 # ------------- the script itself --------------------------------------
+$LOGGER "$0 started"
+
+# Get the dirrectory relative to this script
+BASE_SCRIPT_DIR="${0%/*}"
+
+# Get OrbsUtils full path
+ORBS_UTILS_FULL="${BASE_SCRIPT_DIR}/${ORBS_UTILS}"
+
+# Make sure our shared utils file exists
+if [ ! -f ${ORBS_UTILS_FULL} ]
+then
+        $LOGGER "Cant find ${ORBS_UTILS_FULL}, exiting"
+        exit 1
+fi
+
+# Source our shared utils file
+source ${ORBS_UTILS_FULL}
 
 # Make sure that we are not already running
+cd ${BASE_SCRIPT_DIR}
 testIfAlreadyRunning
 
-$LOGGER "$0 started"
+# Get config files full path
+ORBS_CONF_FULL="${BASE_SCRIPT_DIR}/../config/${ORBS_CONF}"
+
+# Make sure our config file exists
+if [ ! -f ${ORBS_CONF_FULL} ]
+then
+        $LOGGER "Cant find ${ORBS_CONF_FULL}, exiting"
+        exit 1
+fi
+
+# Read in config file values
+SNAPSHOT_RW=`grep 'SNAPSHOT_RW=' ${ORBS_CONF_FULL} | awk -F"=" '{print $2}'`
 
 # Sanity check configuration file variables
 if [ -z ${SNAPSHOT_RW} ]
